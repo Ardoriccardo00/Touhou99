d4f7ff6a4d59186b465794799c51a6669b1b839b
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2019-05-08 13:56:31 +0200

    Aggiunte molte cose
    Come movimento e sparare

---
+++ Touhou99/Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public Transform firePoint2;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Instantiate(bulletPrefab, firePoint2.position, firePoint2.rotation);
+    }
+}
