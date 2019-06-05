ed01986b909b72b3befd8287bfcb5e97f597bf6b
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2019-06-05 13:54:01 +0200

    molte cose
    da finire

---
+++ Touhou99/Assets/Scripts/HostGame.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HostGame : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint roomSize = 6;
+    private string roomName;
+
+    private NetworkManager nm;
+
+    void Start()
+    {
+        nm = NetworkManager.singleton;
+
+        if(nm.matchMaker == null)
+        {
+            nm.StartMatchMaker();
+        }
+    }
+
+    public void setRoomName(string _name)
+    {
+        roomName = _name;
+    }
+
+    public void createRoom()
+    {
+        if(roomName != "" && roomName != null)
+        {
+            Debug.Log("Creating room named " + roomName + " for " + roomSize + " players");
+            nm.matchMaker.CreateMatch(roomName, roomSize, true, "", "", "", 0, 0, nm.OnMatchCreate);
+        }
+    }
+}
