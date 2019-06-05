6735875105956cb485cf722b2f1e961d997ee5fa
Ardoriccardo00 <richieremitico@gmail.com>
2019-06-05 22:26:36 +0200

    conmtinusre

--- Touhou99/Assets/Scripts/JoinGame.cs
+++ Touhou99/Assets/Scripts/JoinGame.cs
@@ -1,18 +1,88 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 public class JoinGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    List<GameObject> roomList = new List<GameObject>();
+
+    [SerializeField]
+    private Text status;
+
+    [SerializeField]
+    private GameObject roomListItemPrefab;
+
+    [SerializeField]
+    private Transform roomListParent;
+
+    [System.Obsolete]
+    private NetworkManager nm;
+
+    [System.Obsolete]
+    private void Start()
     {
-        
+        nm = NetworkManager.singleton;
+
+        if(nm.matchMaker == null)
+        {
+            nm.StartMatchMaker();
+        }
+
+        RefreshRoomList();
     }
 
-    // Update is called once per frame
-    void Update()
+    //[System.Obsolete]
+    public void RefreshRoomList()
     {
-        
+        nm.matchMaker.ListMatches(0, 20, "", false, 0, 0, OnMatchList);
+        status.text = "Loading...";
+    }
+
+    [System.Obsolete]
+    public void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matches)
+    {
+        status.text = "";
+
+        //if(matchList = null)
+        //{
+        //    status.text = "Couldn't get room list";
+        //    return;
+        //}
+
+        ClearRoomList();
+
+        foreach (MatchInfoSnapshot match in matches)
+        {
+            GameObject _roomListItemGO = Instantiate(roomListItemPrefab);
+            _roomListItemGO.transform.SetParent(roomListParent);
+
+            RoomListItem _roomListItem = GetComponent<RoomListItem>();
+
+            if(_roomListItem != null)
+            {
+                _roomListItem.Setup(match);
+            }
+            //bla bla bla tutorial
+            roomList.Add(_roomListItemGO);
+        }
+
+        if(roomList.Count == 0)
+        {
+            status.text = "No rooms at the moment";
+        }
+    }
+
+    void ClearRoomList()
+    {
+        for(int i = 0; i < roomList.Count; i++)
+        {
+            Destroy(roomList[i]);
+        }
+
+        roomList.Clear();
+
     }
 }
---
+++ Touhou99/Assets/Scripts/RoomListItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking.Match;
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField]
+    private Text roomNameText;
+
+    private MatchDesc match;
+
+    public void Setup(MatchDesc _match)
+    {
+        match = _match;
+
+        roomNameText.text = match.name + "(" + match.currentSize + "/" + match.maxSize + ")";
+    }
+}
