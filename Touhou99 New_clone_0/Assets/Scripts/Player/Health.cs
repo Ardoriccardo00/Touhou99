0cf35f9674e4d1bd66e669126769ff86fe102a32
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2021-02-08 21:45:22 +0100

    Bullet Network Movement

---
+++ Touhou99 New_clone_0/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Health : NetworkBehaviour
+{
+    BoxCollider2D hitBox;
+
+    [SerializeField] float maxHealth;
+    float currentHealth;
+
+    void Start()
+    {
+        hitBox = GetComponent<BoxCollider2D>();
+        currentHealth = maxHealth;
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if(collision.gameObject.tag == "Bullet")
+		{
+            float newDamage = collision.gameObject.GetComponent<BulletBehaviour>().bulletDamage;
+            CmdDecreaseHealth(newDamage);
+        }     
+	}
+
+	[Command]
+    public void CmdIncreaseHealth(float value)
+	{
+        RpcIncreaseHealth(value);
+	}
+
+    [ClientRpc]
+    void RpcIncreaseHealth(float value)
+	{
+        currentHealth += value;
+	}
+
+    [Command]
+    public void CmdDecreaseHealth(float value)
+    {
+        RpcDecreaseHealth(value);
+    }
+
+    [ClientRpc]
+    void RpcDecreaseHealth(float value)
+    {
+        currentHealth -= value;
+    }
+
+    [Command]
+    void CmdDie()
+	{
+        RpcDie();
+	}
+
+    [ClientRpc]
+    void RpcDie()
+	{
+        Destroy(gameObject);
+	}
+}
