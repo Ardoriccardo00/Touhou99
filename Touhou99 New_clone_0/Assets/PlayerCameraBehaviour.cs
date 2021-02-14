4549d808b6f08a74f688abea73794fa23054cbdd
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2021-02-14 17:16:16 +0100

    Cameras are now working yay

--- Touhou99 New_clone_0/Assets/PlayerCameraBehaviour.cs
+++ Touhou99 New_clone_0/Assets/PlayerCameraBehaviour.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 public class PlayerCameraBehaviour : NetworkBehaviour
 {
     Vector3 position;
-    public bool stayFixed = true;
 
-    void Awake()
+    public void SetPosition(Vector3 newPos)
+	{
+        position = newPos;
+	}
+
+	void LateUpdate()
     {
-        position = transform.position;
-    }
-    void LateUpdate()
-    {
-        if (stayFixed) transform.position = new Vector3(position.x, position.y, -.5f);
-        else position = FindObjectOfType<PlayerIdentity>().transform.position;
+        transform.position = new Vector3(position.x, position.y, -.5f);
     }
 
     void Update()
